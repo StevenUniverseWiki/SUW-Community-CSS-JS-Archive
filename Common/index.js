a3fd806776e456467808f85c0d2e4f754d97ecdf
TheNozomi <13172293+TheNozomi@users.noreply.github.com>
2023-09-08 00:08:50 -0300

    Backup files from mw

---
+++ Common/index.js
@@ -0,0 +1,56 @@
+(function(window, $, mw) {
+    'use strict';
+    function mediaTags($content) {
+      $content.find('.html5-audio').each(function() {
+        var esc = mw.html.escape,
+          $this = $(this),
+          options = esc($this.attr('data-options') || ''),
+          src = esc($this.attr('data-src') || ''),
+          type = esc($this.attr('data-type') || '');
+        $this.html('<audio ' + options + '><source src="' + src + '" type="' + type + '"></audio>');
+      });
+      $content.find('.html5-video').each(function() {
+        var esc = mw.html.escape,
+          $this = $(this),
+          width = esc($this.attr('data-width') || ''),
+          height = esc($this.attr('data-height') || ''),
+          options = esc($this.attr('data-options') || ''),
+          src = esc($this.attr('data-src') || ''),
+          type = esc($this.attr('data-type') || '');
+        $this.html(
+          '<video width="' +
+            width +
+            '" height="' +
+            height +
+            '" ' +
+            options +
+            '><source src="' +
+            src +
+            '" type="' +
+            type +
+            '"></video>'
+        );
+      });
+    }
+    mw.hook('wikipage.content').add(mediaTags);
+  })(window, jQuery, mediaWiki);
+  
+  $('.norc').bind('contextmenu', function(e) {
+      return false;
+  });
+  
+  /** Load CssVars.js on IE **/
+  if (/Trident|MSIE/.test(navigator.userAgent)) {
+      $.ajax({
+          cache: true,
+          dataType: "script",
+          url: '/es/load.php?mode=articles&only=scripts&articles=MediaWiki:CssVars.js'
+      }).done(function() {
+          cssVars({
+              include: 'style,link[href*="/load.php"]',
+              onlyLegacy    : true,
+              watch         : true
+          });        
+      });    
+  }
+  
